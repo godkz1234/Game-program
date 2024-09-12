c28971a026ed424f83d9b1141e7454485dbec0dd
godkz1234 <ddddeong@naver.com>
2024-09-12 21:46:24 +0900

    (2024.09.12) 더블 버퍼링

--- program/Source.c
+++ program/Source.c
@@ -1,5 +1,73 @@
 #include <stdio.h>
 #include <windows.h>
+#include <conio.h> 
+#include <string.h>
+
+#define UP 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
+
+int screenIndex;
+HANDLE screen[2];
+
+void Initialize()
+{
+	CONSOLE_CURSOR_INFO cursor;
+
+	// 화면 버퍼를 2개 생성합니다.
+	screen[0] = CreateConsoleScreenBuffer
+	(
+		GENERIC_READ | GENERIC_WRITE,
+		0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL
+	);
+
+	screen[1] = CreateConsoleScreenBuffer
+	(
+		GENERIC_READ | GENERIC_WRITE,
+		0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL
+	);
+	
+	cursor.dwSize = 1;
+	cursor.bVisible = FALSE;
+	SetConsoleCursorInfo(screen[0], &cursor);
+	SetConsoleCursorInfo(screen[1], &cursor);
+}
+
+void Flip()
+{
+	SetConsoleActiveScreenBuffer(screen[screenIndex]);
+
+	screenIndex = !screenIndex;
+}
+
+void Clear() 
+{
+	COORD position = { 0,0 };
+
+	DWORD dword;
+
+	FillConsoleOutputCharacter
+	(
+		screen[screenIndex], ' ', 50 * 20, position, &dword
+	);
+
+}
+
+void Release()
+{
+	CloseHandle(screen[0]);
+	CloseHandle(screen[1]);
+}
+
+void Render(int x, int y, const char * string)
+{
+	DWORD dword;
+	COORD position = { x, y };
+
+	SetConsoleCursorPosition(screen[screenIndex], position);
+	WriteFile(screen[screenIndex], string, strlen(string), &dword, NULL);
+}
 
 void Position(int x, int y)
 {
@@ -11,17 +79,85 @@ void Position(int x, int y)
 }
 
 int main()
-{
+{	
+	char key = 0;
+	int x = 0;
+	int y = 0;
+
+	Initialize();
+
 	while (1)
 	{
-		Position(10, 10);
-		printf("*");
+		Flip();
 
-		Position(5, 5);
-		printf("@");
+		Clear();
 
-		system("cls");
+
+			
+		if (_kbhit())
+		{
+			key = _getch();
+
+			if (key == -32)
+			{
+				key = _getch();
+			}
+
+			switch (key)
+			{
+			case UP: // printf("UP\n");
+				y--;
+				break;
+
+			case LEFT: // printf("LEFT\n");
+				x -= 2;
+				break;
+
+			case RIGHT: // printf("RIGHT\n");
+				x += 2;
+				break;
+
+			case DOWN: // printf("DOWN\n");
+				y++;
+				break;
+
+			default: // printf("Exception\n");
+				break;
+			}
+
+		}
+
+		Render(x, y, "*");
+
 	}
+
+		
+
+
+		// if (key == 72)
+		// {
+		// 	printf("UP\n");
+		// }
+		// if (key == 75)
+		// {
+		// 	printf("LEFT\n");
+		// }
+		// if (key == 77)
+		// {
+		// 	printf("RIGHT\n");
+		// }
+		// if (key == 80)
+		// {
+		// 	printf("DOWN\n");
+		// }
+
+
+		
+		// Position(10, 10);
+		// printf("*");
+
+		// system("cls");
+	
 	
 	return 0;
 }
